152a72027ff2efa3c9add910fe324383d19eab06
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-03 14:33:55 +0200

    Es1

---
+++ src/Es1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Es1 {
+
+    public static boolean strEvenOdd (String str) {
+        int n = str.length();
+        return n % 2 == 0;
+    }
+
+public static boolean leepYear(int year) {
+        if (year % 4==0 ) {
+            if (year % 100 ==0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+
+        return false;
+    }
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+        String str;
+        int year;
+        System.out.println("inserisci una stringa");
+        str = input.nextLine();
+        System.out.println("il numero di caratteri di " + str + " è " + (strEvenOdd(str)? "pari" : "dispari"));
+
+        System.out.println("inserisci un anno");
+        year = input.nextInt();
+        System.out.println(year + (leepYear(year)? " è bisestile" : " non è bisestile"));
+        input.close();
+    }
+
+}
