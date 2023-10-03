fed1b3f20b96709a01cbd7570f5aa9ef55785f7e
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-03 13:06:35 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
