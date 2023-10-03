96289fa76e45da858976c86cde8ac530fa04dd9f
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-03 15:02:33 +0200

    Es3

---
+++ src/Es3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Es3 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean quit = false;
+        while(!quit) {
+            String str;
+            System.out.println("inserisci una stringa");
+            str= input.nextLine();
+            if (str.equals(":q")) {
+                System.out.println("exit.");
+                quit = true;
+            }
+            else {
+                str = str.replaceAll("", ",") ;
+                str = str.substring(1, str.length() - 1);
+                System.out.println(str);
+            }
+        }
+
+
+    }
+
+
+}
